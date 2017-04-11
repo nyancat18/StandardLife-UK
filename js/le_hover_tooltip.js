d4c4d240ac4258ae7228ebdea5b8352000c70192
mikhailarden <mikhail@studioarden.com>
2017-04-11 16:12:29 -0500

    commit

--- js/le_hover_tooltip.js
+++ js/le_hover_tooltip.js
@@ -2,21 +2,39 @@
 $(document).ready(function(){
      //$('img[usemap]').maphilight();
      var $dom_tooltip = $(".tehj-tooltip");
-     var mouseX;
-     var mouseY;
+     //var mouseX;
+     //var mouseY;
 
-     var mouseOffsetX = 20;
-     var mouseOffsetY = 10;
+    // var mouseOffsetX = 20;
+     //var mouseOffsetY = 10;
 
      $(document).mousemove(function(e){
-      mouseX =  e.pageX;
-      mouseY = (e.pageY - window.pageYOffset);
+      //mouseX =  e.pageX;
+     // mouseY = (e.pageY - window.pageYOffset);
    });
 
+   $dom_tooltip.html($($('.tehj-has-tooltip.active').data("tehj-tooltip-data")).html());
+
      $('.tehj-has-tooltip').hover(function(){
-          $dom_tooltip.find("p").html($($(this).data("tehj-tooltip-data")).html());
-          //alert($(this).data("tehj-tooltip-data"));
-          $dom_tooltip.fadeIn("fast");
+          if(!$(this).hasClass("active")){
+               $(this).addClass("active");
+               $dom_tooltip.html($($(this).data("tehj-tooltip-data")).html());
+          }else{
+               $dom_tooltip.html($($(this).data("tehj-tooltip-data")).html());
+          }
+     }, function(){
+          if($(this).hasClass("active")){
+               $(this).removeClass("active");
+          }
+          //$dom_tooltip.fadeOut("fast");
+          //$dom_tooltip.find("p").html("");
+     });
+
+     /*$(".tehj-has-tooltip.active").hover(function(){
+          //alert($($(this).data("tehj-tooltip-data")).html());
+          $dom_tooltip.html($($(this).data("tehj-tooltip-data")).html());
+         /* //alert($(this).data("tehj-tooltip-data"));
+          //$dom_tooltip.fadeIn("fast");
 
           var areaCoords = $(this).attr("coords");
           areaCoords = areaCoords.split(',');
@@ -29,12 +47,9 @@ $(document).ready(function(){
           //console.log($x+" | "+$y);
           //$dom_tooltip.css({ left: $x + "px", top: $y + "px" });
           if($(this).data("tehj-tooltip-side") == "right"){
-               $dom_tooltip.css({float:'right', right: 0 + "px", top: 0 + "px" });
+               //$dom_tooltip.css({float:'right', right: 0 + "px", top: 0 + "px" });
           }else{
-               $dom_tooltip.css({float: 'left', left: 0 + "px", top: 0 + "px" });
-          }
-     }, function(){
-          $dom_tooltip.fadeOut("fast");
-          $dom_tooltip.find("p").html("");
-     });
+               //$dom_tooltip.css({float: 'left', left: 0 + "px", top: 0 + "px" });
+          }*
+     });*/
 });
